d6a79f65b856d52a927dfaab1630241f77bf7186
ibasoftdeveloper <45536133+ibasoftdeveloper@users.noreply.github.com>
2019-02-22 09:59:51 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
-/**
- * @param preferences - an array of integers. Indices of people, whom they love
- * @returns number of love triangles
- */
-module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
-};
+module.exports = function getLoveTrianglesCount(preferences) {
+	
+
+let count = 0;
+ 
+  
+  for (let i = 0; i < preferences.length; i++) {
+
+
+  if (i+1 === preferences[preferences[preferences[i] - 1] - 1]) {
+    count++;
+  
+  }
+
+  }
+  return Math.floor(count/3); 
+
+}
